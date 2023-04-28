dcd9ac9da3ebb18a391e55bf8af06099b901dcd8
Cen <54569460+KenKout@users.noreply.github.com>
2023-04-28 20:42:27 +0700

    Create Aldente.js

---
+++ scripts/langkhach/Aldente.js
@@ -0,0 +1,2 @@
+const obj = {"success":true,"response":{"expires":false,"expiry_date":null,"product_id":"647568","type":"activation_license","allowed_uses":"3","times_used":"1"}};
+$done({status:200,headers:{'Content-Type':'application/json'},body:JSON.stringify(obj)});
